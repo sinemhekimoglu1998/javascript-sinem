40d9035f8b75de471cfbdca5d7fe75291ef6e838
sinemhkmglu <sinem.hkmglu@gmail.com>
2019-12-17 15:05:10 +0100

    added alert

---
+++ 02-maths/01-calculator-one/script.js
@@ -0,0 +1,32 @@
+/* becode/javascript
+ *
+ * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+
+    document.getElementById("addition").addEventListener("click", function() {
+        let firstArgument = document.getElementById("op-one").value;
+        let secondArgument = document.getElementById("op-two").value;
+        alert("")
+    });
+
+    document.getElementById("substraction").addEventListener("click", function() {
+        alert("test")
+    });
+
+    document.getElementById("multiplication").addEventListener("click", function() {
+     alert("test")
+    });
+
+    document.getElementById("division").addEventListener("click", function() {
+       alert("test")
+    });
+})();
